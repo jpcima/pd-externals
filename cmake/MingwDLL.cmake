5071f516da572ffa86a41590aa6bc09f1b11425e
JP Cimalando <jpcima@users.noreply.github.com>
2018-02-07 04:29:01 +0100

    fix windows linker problem

--- cmake/MingwDLL.cmake
+++ cmake/MingwDLL.cmake
@@ -3,9 +3,9 @@
 #
 
 macro(mingw_link_static_system_libs TARGET)
-  if(CMAKE_SYSTEM_NAME MATCHES Windows AND
-      (CMAKE_C_COMPILER_ID MATCHES GNU OR CMAKE_C_COMPILER_ID MATCHES Clang))
-      set_property(TARGET "${TARGET}" APPEND_STRING PROPERTY
-        LINK_FLAGS " -static-libgcc -static-libstdc++ -Wl,-Bstatic,--whole-archive -lwinpthread -Wl,-Bdynamic,--no-whole-archive")
+  if(CMAKE_SYSTEM_NAME MATCHES "Windows" AND
+      (CMAKE_C_COMPILER_ID MATCHES "GNU" OR CMAKE_C_COMPILER_ID MATCHES "Clang"))
+    set_property(TARGET "${TARGET}" APPEND_STRING PROPERTY
+      LINK_FLAGS " -static-libgcc -static-libstdc++ -Wl,-Bstatic,--whole-archive -lwinpthread -Wl,-Bdynamic,--no-whole-archive")
   endif()
 endmacro()
