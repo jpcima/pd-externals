76bfc02fc6767ba776a21224b615d7093c1b91b1
JP Cimalando <jpcima@users.noreply.github.com>
2018-03-04 16:39:13 +0100

    darwin linker flags

--- cmake/PdExternal.cmake
+++ cmake/PdExternal.cmake
@@ -92,6 +92,10 @@ macro(add_pd_external TARGET)
     LIBRARY_OUTPUT_DIRECTORY "${PROJECT_SOURCE_DIR}"
     PREFIX ""
     SUFFIX "${PD_EXTERNAL_SUFFIX}")
+  if(CMAKE_SYSTEM_NAME MATCHES "Darwin")
+    set_target_properties("${TARGET}" PROPERTIES
+      LINK_FLAGS "-Wl,-undefined,suppress,-flat_namespace")
+  endif()
   mingw_link_static_system_libs("${TARGET}")
   unset(_name)
 endmacro()
